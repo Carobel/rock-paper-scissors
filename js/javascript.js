4d97926b982f707b2f291f07cea4171238021173
Caroline <61837243+Carobel@users.noreply.github.com>
2025-08-22 10:54:36 +0200

    add getComputerChoice function
    Wrote function that outputs a string containing rock, paper, or scissors.

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+
+function getComputerChoice() {
+    rand = Math.random();
+    if (rand < 0.333) {
+        return "rock"
+    } else if (rand < 0.666) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
